b1945188f3f9c28e2cc53181d24462d355f35c31
Felipe <feli20gutierrez@hotmail.com>
2023-04-12 12:24:33 -0500

    Fixing indentation issues.

--- main.cpp
+++ main.cpp
@@ -414,73 +414,74 @@ void renderCube()
 
 void EditTransform(float* cameraView, float* cameraProjection, float* matrix, bool editTransformDecomposition, ImGuiIO& io)
 {
-   static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::LOCAL);
-   static bool useSnap = false;
-   static float snap[3] = { 1.f, 1.f, 1.f };
-   static float bounds[] = { -0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f };
-   static float boundsSnap[] = { 0.1f, 0.1f, 0.1f };
-   static bool boundSizing = false;
-   static bool boundSizingSnap = false;
+    ImGuizmo::SetRect(0, 0, io.DisplaySize.x/2, io.DisplaySize.y);
+    static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::LOCAL);
+    static bool useSnap = false;
+    static float snap[3] = { 1.f, 1.f, 1.f };
+    static float bounds[] = { -0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f };
+    static float boundsSnap[] = { 0.1f, 0.1f, 0.1f };
+    static bool boundSizing = false;
+    static bool boundSizingSnap = false;
 
-   if (editTransformDecomposition)
-   {
-      if (ImGui::IsKeyPressed(ImGuiKey_T))
-         mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
-      if (ImGui::IsKeyPressed(ImGuiKey_R))
-         mCurrentGizmoOperation = ImGuizmo::ROTATE;
-      if (ImGui::IsKeyPressed(ImGuiKey_E)) 
-         mCurrentGizmoOperation = ImGuizmo::SCALE;
-      if (ImGui::RadioButton("Translate", mCurrentGizmoOperation == ImGuizmo::TRANSLATE))
-         mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
-      ImGui::SameLine();
-      if (ImGui::RadioButton("Rotate", mCurrentGizmoOperation == ImGuizmo::ROTATE))
-         mCurrentGizmoOperation = ImGuizmo::ROTATE;
-      ImGui::SameLine();
-      if (ImGui::RadioButton("Scale", mCurrentGizmoOperation == ImGuizmo::SCALE))
-         mCurrentGizmoOperation = ImGuizmo::SCALE;
-      if (ImGui::RadioButton("Universal", mCurrentGizmoOperation == ImGuizmo::UNIVERSAL))
-         mCurrentGizmoOperation = ImGuizmo::UNIVERSAL;
-      float matrixTranslation[3], matrixRotation[3], matrixScale[3];
-      ImGuizmo::DecomposeMatrixToComponents(matrix, matrixTranslation, matrixRotation, matrixScale);
-      ImGui::InputFloat3("Tr", matrixTranslation);
-      ImGui::InputFloat3("Rt", matrixRotation);
-      ImGui::InputFloat3("Sc", matrixScale);
-      ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, matrix);
+    if (editTransformDecomposition)
+    {
+        if (ImGui::IsKeyPressed(ImGuiKey_T))
+            mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
+        if (ImGui::IsKeyPressed(ImGuiKey_R))
+            mCurrentGizmoOperation = ImGuizmo::ROTATE;
+        if (ImGui::IsKeyPressed(ImGuiKey_E)) 
+            mCurrentGizmoOperation = ImGuizmo::SCALE;
+        if (ImGui::RadioButton("Translate", mCurrentGizmoOperation == ImGuizmo::TRANSLATE))
+            mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
+        ImGui::SameLine();
+        if (ImGui::RadioButton("Rotate", mCurrentGizmoOperation == ImGuizmo::ROTATE))
+            mCurrentGizmoOperation = ImGuizmo::ROTATE;
+        ImGui::SameLine();
+        if (ImGui::RadioButton("Scale", mCurrentGizmoOperation == ImGuizmo::SCALE))
+            mCurrentGizmoOperation = ImGuizmo::SCALE;
+        if (ImGui::RadioButton("Universal", mCurrentGizmoOperation == ImGuizmo::UNIVERSAL))
+            mCurrentGizmoOperation = ImGuizmo::UNIVERSAL;
+        float matrixTranslation[3], matrixRotation[3], matrixScale[3];
+        ImGuizmo::DecomposeMatrixToComponents(matrix, matrixTranslation, matrixRotation, matrixScale);
+        ImGui::InputFloat3("Tr", matrixTranslation);
+        ImGui::InputFloat3("Rt", matrixRotation);
+        ImGui::InputFloat3("Sc", matrixScale);
+        ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, matrix);
 
-      if (mCurrentGizmoOperation != ImGuizmo::SCALE)
-      {
-         if (ImGui::RadioButton("Local", mCurrentGizmoMode == ImGuizmo::LOCAL))
-            mCurrentGizmoMode = ImGuizmo::LOCAL;
-         ImGui::SameLine();
-         if (ImGui::RadioButton("World", mCurrentGizmoMode == ImGuizmo::WORLD))
-            mCurrentGizmoMode = ImGuizmo::WORLD;
-      }
-      if (ImGui::IsKeyPressed(ImGuiKey_P))
-         useSnap = !useSnap;
-      ImGui::Checkbox("##UseSnap", &useSnap);
-      ImGui::SameLine();
+        if (mCurrentGizmoOperation != ImGuizmo::SCALE)
+        {
+            if (ImGui::RadioButton("Local", mCurrentGizmoMode == ImGuizmo::LOCAL))
+                mCurrentGizmoMode = ImGuizmo::LOCAL;
+            ImGui::SameLine();
+            if (ImGui::RadioButton("World", mCurrentGizmoMode == ImGuizmo::WORLD))
+                mCurrentGizmoMode = ImGuizmo::WORLD;
+        }
+        if (ImGui::IsKeyPressed(ImGuiKey_P))
+            useSnap = !useSnap;
+        ImGui::Checkbox("##UseSnap", &useSnap);
+        ImGui::SameLine();
 
-      switch (mCurrentGizmoOperation)
-      {
-      case ImGuizmo::TRANSLATE:
-         ImGui::InputFloat3("Snap", &snap[0]);
-         break;
-      case ImGuizmo::ROTATE:
-         ImGui::InputFloat("Angle Snap", &snap[0]);
-         break;
-      case ImGuizmo::SCALE:
-         ImGui::InputFloat("Scale Snap", &snap[0]);
-         break;
-      }
-      ImGui::Checkbox("Bound Sizing", &boundSizing);
-      if (boundSizing)
-      {
-         ImGui::PushID(3);
-         ImGui::Checkbox("##BoundSizing", &boundSizingSnap);
-         ImGui::SameLine();
-         ImGui::InputFloat3("Snap", boundsSnap);
-         ImGui::PopID();
-      }
+        switch (mCurrentGizmoOperation)
+        {
+        case ImGuizmo::TRANSLATE:
+            ImGui::InputFloat3("Snap", &snap[0]);
+            break;
+        case ImGuizmo::ROTATE:
+            ImGui::InputFloat("Angle Snap", &snap[0]);
+            break;
+        case ImGuizmo::SCALE:
+            ImGui::InputFloat("Scale Snap", &snap[0]);
+            break;
+        }
+        ImGui::Checkbox("Bound Sizing", &boundSizing);
+        if (boundSizing)
+        {
+            ImGui::PushID(3);
+            ImGui::Checkbox("##BoundSizing", &boundSizingSnap);
+            ImGui::SameLine();
+            ImGui::InputFloat3("Snap", boundsSnap);
+            ImGui::PopID();
+        }
    }
 
    //ImGuiIO& io = ImGui::GetIO();
